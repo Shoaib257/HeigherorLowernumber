065b8b1ce306d191712ef8f258f9e7d43d02686b
Shoaib257 <shoaib.uddin.bhuiyan@gmail.com>
2020-03-10 15:41:59 +0600

    Initial commit

---
+++ app/src/main/java/com/example/heigherorlowernumber/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.heigherorlowernumber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    int randomNumber;
+
+    public void generateRandomNumber(){
+        Random rand=new Random();
+
+        randomNumber=rand.nextInt(50)+1;
+    }
+
+    public void guess(View view){
+        EditText editText=findViewById(R.id.editText2);
+        int guessValue=Integer.parseInt(editText.getText().toString());
+
+        String message;
+
+        if (guessValue>randomNumber){
+           message="Lower";
+        }else if (guessValue<randomNumber){
+            message="Heigher";
+        }else {
+            message="You got it! try again?";
+        }
+
+        Toast.makeText(this, "", Toast.LENGTH_LONG).show();
+
+        Log.i("Entered Value",editText.getText().toString());
+
+        Log.i("Random Number",Integer.toString(randomNumber));
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        generateRandomNumber();
+    }
+}
